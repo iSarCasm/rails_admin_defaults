0fcc00a3cfdbd8134e287fffb0d898b190bce650
SarCasm <sarcasm008@gmail.com>
2017-10-20 23:30:20 +0300

    Rename defaults_action to defaults

--- lib/rails_admin_defaults/defaults_action.rb
+++ lib/rails_admin_defaults/defaults_action.rb
@@ -4,7 +4,7 @@ require 'rails_admin/config/actions/base'
 module RailsAdmin
   module Config
     module Actions
-      class DefaultsAction < RailsAdmin::Config::Actions::Base
+      class Defaults < RailsAdmin::Config::Actions::Base
         RailsAdmin::Config::Actions.register(self)
 
         register_instance_option :visible? do
